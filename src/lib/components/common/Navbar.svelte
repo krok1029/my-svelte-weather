56726bf9356aec0a3d176609b642f38a2739aabe
leo lee <krok1029@gmail.com>
2025-08-08 12:17:22 +0800

    chore: format

--- src/lib/components/common/Navbar.svelte
+++ src/lib/components/common/Navbar.svelte
@@ -17,26 +17,26 @@
 	>
 		<a href="##" class="flex items-center gap-2 text-lg font-semibold md:text-base">
 			<CloudSun class="h-6 w-6" />
-                        <span class="sr-only">Svelte Weather App</span>
+			<span class="sr-only">Svelte Weather App</span>
 		</a>
 		<a href="##" class="text-foreground transition-colors hover:text-foreground"> Weather </a>
 	</nav>
 	<Sheet.Root>
-		<Sheet.Trigger asChild >
+		<Sheet.Trigger asChild>
 			{#snippet children({ builder })}
-						<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
+				<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
 					<Menu class="h-5 w-5" />
 					<span class="sr-only">Toggle navigation menu</span>
 				</Button>
-								{/snippet}
-				</Sheet.Trigger>
+			{/snippet}
+		</Sheet.Trigger>
 		<Sheet.Content side="left">
 			<nav class="grid gap-6 text-lg font-medium">
 				<a href="##" class="flex items-center gap-2 text-lg font-semibold">
 					<CloudSun class="h-6 w-6" />
-                                        <span class="sr-only">Svelte Weather App</span>
+					<span class="sr-only">Svelte Weather App</span>
 				</a>
-                                <a href="##" class="hover:text-foreground"> Weather </a>
+				<a href="##" class="hover:text-foreground"> Weather </a>
 			</nav>
 		</Sheet.Content>
 	</Sheet.Root>
@@ -53,19 +53,19 @@
 				</div>
 			</form> -->
 		<DropdownMenu.Root>
-			<DropdownMenu.Trigger asChild >
+			<DropdownMenu.Trigger asChild>
 				{#snippet children({ builder })}
-								<Button builders={[builder]} variant="outline" size="icon">
+					<Button builders={[builder]} variant="outline" size="icon">
 						<Sun
-							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+							class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
 						/>
 						<Moon
-							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+							class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
 						/>
 						<span class="sr-only">Toggle theme</span>
 					</Button>
-											{/snippet}
-						</DropdownMenu.Trigger>
+				{/snippet}
+			</DropdownMenu.Trigger>
 			<DropdownMenu.Content align="end">
 				<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
 				<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
@@ -73,14 +73,14 @@
 			</DropdownMenu.Content>
 		</DropdownMenu.Root>
 		<DropdownMenu.Root>
-			<DropdownMenu.Trigger asChild >
+			<DropdownMenu.Trigger asChild>
 				{#snippet children({ builder })}
-								<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
+					<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
 						<CircleUser class="h-5 w-5" />
 						<span class="sr-only">Toggle user menu</span>
 					</Button>
-											{/snippet}
-						</DropdownMenu.Trigger>
+				{/snippet}
+			</DropdownMenu.Trigger>
 			<DropdownMenu.Content align="end">
 				<DropdownMenu.Label>My Account</DropdownMenu.Label>
 				<DropdownMenu.Separator />
